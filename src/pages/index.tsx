0e51de464a82f0ed19c374b68e46c6b6a00c9d2d
ziya <ziyaelarief@gmail.com>
2022-08-21 11:10:46 +0700

    feat: use data from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,104 +3,60 @@ import React, { useEffect, useState } from 'react';
 import Layout from '@/components/layout/Layout';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import Seo from '@/components/Seo';
-const latteArtDrinks = [
-  'Piccollo',
-  'Brown Latte',
-  'Hot Capucino',
-  // 'Hot Mocha',
-  'Cortado (Double shot)',
-  'Flat White (Double shot)',
-  'Cafe Latte (200/275ml) (Double Shot)',
-  'Goth Latte',
-];
-const types = [
-  ...latteArtDrinks,
-  'Iced Latte',
-  // 'Iced Americano',
-  'Es Kopi Susu',
-  // 'Espresso',
-  // 'Iced Mocha',
-  'Affogato',
-  'Tubruk',
-  'Filter Coffee',
-  'Japanese Filter',
-];
+
 const arts = [
-  //'Heart',
-  'Tulip',
-  'Rosetta',
-  'Winged Heart',
-  'Rippled Heart',
-  'Winged Tulip',
-  'Slosetta',
-  'Slow Rippled Heart',
-];
-const beans = ['arabica', 'blend'];
-
-const esKopiSusuSweeteners = [
-  'Coconut Delight',
-  'Stevia',
-  'Sari Tebu',
-  'Brown Sugar',
+  { name: 'Heart', score: 1 },
+  { name: 'Tulip', score: 5 },
+  { name: 'Rosetta', score: 5 },
+  { name: 'Winged Heart', score: 5 },
+  { name: 'Rippled Heart', score: 3 },
+  { name: 'Winged Tulip', score: 5 },
+  { name: 'Slosetta', score: 5 },
+  { name: 'Slow Rippled Heart', score: 4 },
 ];
 
-export default function HomePage() {
-  const [drinkType, setDrinktype] = useState('');
-  const [artDesign, setArtDesign] = useState('');
-  const [kopsusSweetener, setKopsusSweetener] = useState('');
-  const [beanVariant, setBeanVariant] = useState('');
+const beans = [
+  { name: 'arabica', score: 5 },
+  { name: 'blend', score: 3 },
+];
 
-  function setString(
-    setFunction: React.Dispatch<React.SetStateAction<string>>,
-    arrayToBeRandomized: string[]
-  ) {
-    setFunction(
-      arrayToBeRandomized[
-        Math.floor(Math.random() * arrayToBeRandomized.length)
-      ]
-    );
-  }
+type Drink = {
+  name: string;
+  art: boolean;
+  cups: Array<string>;
+  score: number;
+};
+
+export default function HomePage({ drinks }: { drinks: Array<Drink> }) {
+  const [drinkType, setDrinktype] = useState<Drink>();
+  const [artDesign, setArtDesign] = useState<typeof arts[number]>();
+  const [beanVariant, setBeanVariant] = useState<typeof beans[number]>();
 
   function getDrink() {
-    setString(setDrinktype, types);
-    setString(setBeanVariant, beans);
+    setRandom(setDrinktype, drinks);
+    setRandom(setBeanVariant, beans);
   }
 
   useEffect(() => {
-    if (latteArtDrinks.includes(drinkType)) {
-      setString(setArtDesign, arts);
+    if (drinkType && drinkType.art) {
+      setRandom(setArtDesign, arts);
     } else {
-      setArtDesign('');
-    }
-
-    if (drinkType == 'Es Kopi Susu') {
-      setString(setKopsusSweetener, esKopiSusuSweeteners);
-    } else {
-      setKopsusSweetener('');
+      setArtDesign(undefined);
     }
   }, [drinkType]);
 
   return (
     <Layout>
       <Seo />
-
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
             <h1 className='mt-4'>Hi, Let&apos;s Make a Coffee ☕</h1>
-            <div>
-              {drinkType == '' ? (
-                ''
-              ) : (
-                <div>
-                  Our Suggestion: <br />
-                  Drink: {drinkType} <br />
-                  Bean: {beanVariant} <br />
-                  {artDesign && <span>Art: {artDesign}</span>} <br />
-                  {kopsusSweetener && <span>Sweetener: {kopsusSweetener}</span>}
-                </div>
-              )}
-            </div>
+            <Suggestion
+              drinkType={drinkType}
+              artDesign={artDesign}
+              beanVariant={beanVariant}
+            />
             <button
               type='button'
               className='mt-6 mr-2 mb-2 rounded-lg bg-gradient-to-r from-green-400 via-green-500 to-green-600 px-5 py-2.5 text-center text-sm font-medium text-white shadow-lg shadow-green-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-green-300 dark:shadow-lg dark:shadow-green-800/80 dark:focus:ring-green-800'
@@ -120,4 +76,66 @@ export default function HomePage() {
       </main>
     </Layout>
   );
+}
+
+function setRandom<T>(
+  setFunction: React.Dispatch<React.SetStateAction<T | undefined>>,
+  arrayToBeRandomized: T[]
+) {
+  setFunction(
+    arrayToBeRandomized[Math.floor(Math.random() * arrayToBeRandomized.length)]
+  );
+}
+
+type SuggestionProps = {
+  drinkType?: Drink;
+  beanVariant?: typeof beans[number];
+  artDesign?: typeof arts[number];
+};
+
+function Suggestion({ drinkType, beanVariant, artDesign }: SuggestionProps) {
+  if (!drinkType) return null;
+
+  return (
+    <div>
+      Our Suggestion: <br />
+      Drink: {drinkType.name} <br />
+      Bean: {beanVariant?.name} <br />
+      {artDesign && <span>Art: {artDesign.name}</span>}
+    </div>
+  );
+}
+
+type DrinksData = {
+  data: {
+    drinks: Array<Drink>;
+  };
+};
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        {
+          drinks {
+            name
+            art
+            cups
+            score
+          }
+        }
+      `,
+      variables: {},
+    }),
+  });
+
+  const resJson: DrinksData = await res.json();
+
+  return {
+    props: { drinks: resJson.data.drinks },
+  };
 }
